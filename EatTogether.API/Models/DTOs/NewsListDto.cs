746250e6ea055d145cab1faa034f525337577fe0
Weiyan-99 <g2301149040@gmail.com>
2026-04-15 16:34:44 +0800

    feature:最新消息api 文章列表、單篇文章建立

---
+++ EatTogether.API/Models/DTOs/NewsListDto.cs
@@ -0,0 +1,23 @@
+﻿namespace EatTogether.API.Models.DTOs
+{
+
+	public class NewsListDto
+	{
+		public int Id { get; set; }
+
+		public string CategoryName { get; set; }  // 文章類別名稱
+
+		public string Title { get; set; }
+
+		public string Summary { get; set; }   // 存最新消息的文章摘要
+
+		public string CoverImageUrl { get; set; }
+
+		public DateTime? PublishDate { get; set; }
+
+		public bool IsPinned { get; set; }
+
+		public int ViewCount { get; set; }
+
+	}
+}
