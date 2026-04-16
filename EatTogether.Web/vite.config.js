9421f8f0bf9d5e276a4531fe342daa5dd44d9e5f
Amy <amy6072698@gmail.com>
2026-04-17 00:16:15 +0800

    前台環境變數修正

--- EatTogether.Web/vite.config.js
+++ EatTogether.Web/vite.config.js
@@ -1,23 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://localhost:7119',
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
             },
         },
-    },
+        server: {
+            proxy: {
+                '/api': {
+                target: env.VITE_API_TARGET,
+                changeOrigin: true,
+                secure: false,
+                },
+            },
+        },
+    }
 })
+
+    
