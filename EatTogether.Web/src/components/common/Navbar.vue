7114eb9c45844d2b5017130fb342e918f0c7a50f
楊 <yang0005111@gmail.com>
2026-04-22 16:18:18 +0800

    新增Menu.vue

--- EatTogether.Web/src/components/common/Navbar.vue
+++ EatTogether.Web/src/components/common/Navbar.vue
@@ -68,6 +68,7 @@ const route = useRoute()
 const navLinks = [
   { label: '菜單',   to: '/menu' },
   { label: '套餐',   to: '/setmeal' },
+  { label: '本季限定', to: '/limited' },
   { label: '訂位',   to: '/reservation' },
   { label: '會員',   to: '/member' },
   { label: '外帶點餐', to: '/takeout' },
