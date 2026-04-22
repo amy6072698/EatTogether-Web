e13c63de9dd809dc99941eee7df16425a18bb1fc
吳欣柔 <rrr20118@gmail.com>
2026-04-22 16:41:31 +0800

    2

--- EatTogether.API/Program.cs
+++ EatTogether.API/Program.cs
@@ -12,8 +12,8 @@ using System.Threading.RateLimiting;
 
 namespace EatTogether.API
 {
-    public class Program
-    {
+    public class Program  
+	{
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -103,26 +103,17 @@ namespace EatTogether.API
             //builder.Services.AddScoped<IAuthService, AuthService>();
             //builder.Services.AddScoped<IMemberService, MemberService>();
 
-            // 設定 6. Repositories
-            //builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-            //builder.Services.AddScoped<IMemberRepository, MemberRepository>();
-            //builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
-            // 註冊Repository
-            builder.Services.AddScoped<ICouponRepository, CouponRepository>();
-            builder.Services.AddScoped<IEventRepository, EventRepository>();
-            builder.Services.AddScoped<IDishRepository, DishRepository>();
-            builder.Services.AddScoped<IProductRepository, ProductRepository>();
-            builder.Services.AddScoped<ISetMealRepository, SetMealRepository>();
-            builder.Services.AddScoped<ITableRepository, TableRepository>();
-            builder.Services.AddScoped<IMemberCouponRepository, MemberCouponRepository>();
-            builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-            builder.Services.AddScoped<IPasswordResetTokenRepository, PasswordResetTokenRepository>();
-            builder.Services.AddScoped<IPreOrderRepository, PreOrderRepository>();
-            builder.Services.AddScoped<IProductRepository, ProductRepository>();
-            builder.Services.AddScoped<IRoleFunctionRepository, RoleFunctionRepository>();
-            builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-            builder.Services.AddScoped<IUserRepository, UserRepository>();
-			builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+			// 設定 6. Repositories
+			//builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+			//builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+			//builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+			// 註冊Repository
+			builder.Services.AddScoped<IDishRepository, DishRepository>();
+			builder.Services.AddScoped<ISetMealRepository, SetMealRepository>();
+
+			// 註冊Service
+			builder.Services.AddScoped<DishService>();
+			builder.Services.AddScoped<SetMealService>();
 
             // 註冊Service
             builder.Services.AddScoped<IAuthService, AuthService>();
